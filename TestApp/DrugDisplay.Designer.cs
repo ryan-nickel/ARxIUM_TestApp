61ff937eb2b7f601f74f2985869d56c5516d8156
Ryan Nickel <ryan.d.nickel@gmail.com>
2020-11-21 15:41:59 -0700

    Initial commit

---
+++ TestApp/DrugDisplay.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace TestApp
+{
+    partial class DrugDisplay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.buttonIncrement = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(10, 12);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(39, 15);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(10, 41);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(40, 15);
+            this.labelCount.TabIndex = 1;
+            this.labelCount.Text = "Count";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(55, 9);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.ReadOnly = true;
+            this.textBoxName.Size = new System.Drawing.Size(216, 23);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(55, 38);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.ReadOnly = true;
+            this.textBoxCount.Size = new System.Drawing.Size(123, 23);
+            this.textBoxCount.TabIndex = 3;
+            // 
+            // buttonIncrement
+            // 
+            this.buttonIncrement.Location = new System.Drawing.Point(184, 38);
+            this.buttonIncrement.Name = "buttonIncrement";
+            this.buttonIncrement.Size = new System.Drawing.Size(87, 23);
+            this.buttonIncrement.TabIndex = 4;
+            this.buttonIncrement.Text = "Increment";
+            this.buttonIncrement.UseVisualStyleBackColor = true;
+            // 
+            // DrugDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.buttonIncrement);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelName);
+            this.Name = "DrugDisplay";
+            this.Size = new System.Drawing.Size(287, 73);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Button buttonIncrement;
+    }
+}
--- TestApp/MainForm.Designer.cs
+++ TestApp/MainForm.Designer.cs
@@ -29,20 +29,108 @@ namespace TestApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanelMenu = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonResetCounts = new System.Windows.Forms.Button();
+            this.buttonShowLog = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.flowLayoutPanelDrugs = new System.Windows.Forms.FlowLayoutPanel();
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanelMenu
+            // 
+            this.flowLayoutPanelMenu.Controls.Add(this.buttonResetCounts);
+            this.flowLayoutPanelMenu.Controls.Add(this.buttonShowLog);
+            this.flowLayoutPanelMenu.Controls.Add(this.buttonExit);
+            this.flowLayoutPanelMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelMenu.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelMenu.Name = "flowLayoutPanelMenu";
+            this.flowLayoutPanelMenu.Size = new System.Drawing.Size(634, 30);
+            this.flowLayoutPanelMenu.TabIndex = 0;
+            // 
+            // buttonResetCounts
+            // 
+            this.buttonResetCounts.Location = new System.Drawing.Point(3, 3);
+            this.buttonResetCounts.Name = "buttonResetCounts";
+            this.buttonResetCounts.Size = new System.Drawing.Size(75, 23);
+            this.buttonResetCounts.TabIndex = 0;
+            this.buttonResetCounts.Text = "Reset Counts";
+            this.buttonResetCounts.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowLog
+            // 
+            this.buttonShowLog.Location = new System.Drawing.Point(84, 3);
+            this.buttonShowLog.Name = "buttonShowLog";
+            this.buttonShowLog.Size = new System.Drawing.Size(75, 23);
+            this.buttonShowLog.TabIndex = 1;
+            this.buttonShowLog.Text = "Show Log";
+            this.buttonShowLog.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(165, 3);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanelDrugs
+            // 
+            this.flowLayoutPanelDrugs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelDrugs.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelDrugs.Name = "flowLayoutPanelDrugs";
+            this.flowLayoutPanelDrugs.Size = new System.Drawing.Size(634, 438);
+            this.flowLayoutPanelDrugs.TabIndex = 1;
+            // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerMain.Name = "splitContainerMain";
+            this.splitContainerMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainerMain.Panel1.Controls.Add(this.flowLayoutPanelMenu);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.flowLayoutPanelDrugs);
+            this.splitContainerMain.Size = new System.Drawing.Size(634, 472);
+            this.splitContainerMain.SplitterDistance = 30;
+            this.splitContainerMain.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(634, 472);
+            this.Controls.Add(this.splitContainerMain);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "TestApp";
+            this.flowLayoutPanelMenu.ResumeLayout(false);
+            this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMenu;
+        private System.Windows.Forms.Button buttonResetCounts;
+        private System.Windows.Forms.Button buttonShowLog;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelDrugs;
+        private System.Windows.Forms.SplitContainer splitContainerMain;
     }
 }
 
